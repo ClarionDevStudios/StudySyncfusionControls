6d8cf28a2e88685e2f1f5c98730d3ae8095ae520
ChenJie <czbestboy@gmail.com>
2026-08-21 16:51:43 +0800

    学习日志（第一天）

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using StudySyncfusion;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace StudySyncfusionControls
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            AssemblyResolver.LicenseKeyLocator.FindandRegisterLicenseKey();
+            //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JAaF1cX2hAYVF2WmFZfVhgdl9CZlZUR2YuP1ZhSXxVdk1hWH9dcndVRmRfUEV9XEY=");
+        }
+    }
+
+}
---
+++ AssemblyResolver.cs
@@ -0,0 +1,54 @@
+﻿using Syncfusion.Licensing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace StudySyncfusion
+{
+    public static class AssemblyResolver
+    {
+        static AssemblyResolver()
+        {
+
+        }
+        public static class LicenseKeyLocator
+        {
+            public static void FindandRegisterLicenseKey()
+            {
+                SyncfusionLicenseProvider.RegisterLicense(FindLicenseKey());
+            }
+
+            /// <summary>
+            /// Helper method to find a syncfusion license key.
+            /// </summary>
+            /// <returns>License Key</returns>
+            private static string FindLicenseKey()
+            {
+                int levelsToCheck = 12;
+                string filePath = @"SyncfusionLicense.txt";
+
+                string rootPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location.Replace(@"file:///", ""));
+
+                if (string.IsNullOrEmpty(rootPath) || string.IsNullOrEmpty(filePath))
+                {
+                    return string.Empty;
+                }
+
+                for (int n = 0; n < levelsToCheck; n++)
+                {
+                    string fileDataPath = System.IO.Path.Combine(rootPath, filePath);
+                    if (System.IO.File.Exists(fileDataPath))
+                        return File.ReadAllText(fileDataPath, Encoding.UTF8);
+                    DirectoryInfo rootDirectory = Directory.GetParent(rootPath);
+                    if (rootDirectory == null)
+                        break;
+                    rootPath = rootDirectory.FullName;
+                }
+                return string.Empty;
+            }
+        }
+
+    }
+}
